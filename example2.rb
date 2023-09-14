c8fa993b1634c3de194dbdc709c3ed75ba39d74c
Eric Clack <eric@bn7.net>
2023-09-14 20:40:35 +0100

    Added note_bars

--- example2.rb
+++ example2.rb
@@ -20,22 +20,51 @@ def sample_pattern(asample, amps, rate: 1, gain: 1)
   end
 end
 
-def bar_patterns(asample, bars, rate: 1, gain: 1)
+def sample_bars(asample, bars, rate: 1, gain: 1)
   bars.split('|') do |amps|
     sample_pattern(asample, amps, rate: rate, gain: gain)
   end
 end
 
+def note_pattern(asynth, bar, amp: 1)
+  use_synth asynth
+  beat_length = get[:beats_in_bar].to_f/bar.length
+  puts beat_length
+  bar.each do |note|
+    if note != 0 then
+      play note, amp: amp
+    end
+    sleep beat_length
+  end
+end
+
+def note_bars(asynth, bars, amp: 1)
+  bars.each do |bar|
+    note_pattern asynth, bar, amp: amp
+  end
+end
+
+
 live_loop :bass do
-  bar_patterns :drum_bass_soft, "555 |555 |5 5 |5"
-  #sample_pattern :drum_bass_soft, "555 ", gain: 1
+  sample_bars :drum_bass_soft, "5354|5 5 "
 end
 
 live_loop :sn do
-  bar_patterns :drum_snare_hard, "  6 |  6 | 2 5|315315"
+  sample_bars :drum_snare_hard,
+    #1.^.2.^.3.^.4.^.
+    "  1 6    1 6 1  | 6 6"
 end
 
 live_loop :hh do
-  bar_patterns :drum_cymbal_closed, " 7 7 7 7"
+  sample_bars :drum_cymbal_closed, " 7 7 7 7"
 end
 
+live_loop :melody do
+  note_bars :chipbass,
+  [
+    [40,    40,    40,    40],
+    [0, 40, 41, 0,  0, 43, 0, 0],
+    [40,    40,    40,    40],
+    [40, 0, 40, 40, 0, 40],
+  ], amp: 0.3
+end
